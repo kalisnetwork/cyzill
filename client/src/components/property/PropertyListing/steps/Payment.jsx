62202d50787a132231ff7676dbbe8e48f08fc1d1
Kalyan <106701723+kalisnetwork@users.noreply.github.com>
2024-01-02 23:50:29 +0530

    Update Payment.jsx

--- client/src/components/property/PropertyListing/steps/Payment.jsx
+++ client/src/components/property/PropertyListing/steps/Payment.jsx
@@ -14,7 +14,7 @@ const Payment = ({ formData, saveFormData }) => {
     const navigate = useNavigate();
 
     const handleSubmit = async () => {
-        const response = await fetch('http://localhost:5000/api/property/properties', {
+        const response = await fetch('https://cyzill-api.onrender.com/api/property/properties', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ ...formData, username: username }) // include username in the form data
